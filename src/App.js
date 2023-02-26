ed803330873ffe87639fe2801217818012f58075
Manassés Raimundo <119222688+manasseisraimundo@users.noreply.github.com>
2023-02-26 15:03:48 +0100

    inplementei um h1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Seja bem-vindo ao React!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar a página.
         </p>
         <a
           className="App-link"
