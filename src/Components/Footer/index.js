0066d59ab50a1b23e6bf8b4bb14e0eb46f17e214
Manassés Raimundo <119222688+manasseisraimundo@users.noreply.github.com>
2023-08-20 15:18:03 +0100

    Update index.js

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,7 +4,6 @@ import './Footer.css'
 const Footer = () => {
     return(
         <footer className="container-footer">
-     
            <p style={{textAlign: "center"}}> &copy; Filmes & Séries | Todos os direitos reservados. Site desenvolvido por <a href='https://github.com/manassesraimundo' target="_blank">Manassés Raimundo</a></p>
         </footer>
     );
