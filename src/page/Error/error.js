f706b9ef07f4833b8429c0e5bfca5d9cd3b1984d
Manassés Raimundo <119222688+manasseisraimundo@users.noreply.github.com>
2023-08-20 17:06:09 +0100

    criação do arquivo error.css

--- src/page/Error/error.js
+++ src/page/Error/error.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'error.css';
 
 const Error = () => {
     return(
-        <div>
+        <div className='container-error'>
             <h2>Erro Página não encontrada</h2>
 
             <div>
